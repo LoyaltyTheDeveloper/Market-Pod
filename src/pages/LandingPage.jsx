53b68d827760022313ac4c8b43f5095369e13b65
Loyalty <lohunmule@gmail.com>
2024-10-27 19:05:53 -0700

    some additions

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -16,6 +16,12 @@ import Footer from '../Components/Footer';
 import marketData from '../index.json';
 import { useState, useEffect } from 'react';
 import { LuClock5 } from "react-icons/lu";
+import "../index.css"
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { MdOutlineArrowBackIos } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+
 
 function LandingPage() {
 
@@ -26,7 +32,7 @@ function LandingPage() {
     .then((response) => response.json())
     .then((data) => setMarketss(data.markets))
     .catch((error) => console.error('Error fetching comments', error))
-}, )
+}, [])
 
   return (<>
   <Navbar/>
@@ -102,61 +108,47 @@ function LandingPage() {
     </div>
     <div>
 
-    {/* {marketData.shops.map((market) => (<>
-    <div key={market.id} className="flex flex-col">
-        <div className="font-bold text-[25px] lg:text-[30px]">Yoruba Road</div>
-        <div className="w-[270px] lg:w-[100%] md:w-[100%] flex flex-wrap">No 34 off GRA, Adelewola Akinwuyi Road, Ilorin, Kwara State.</div>
-        </div>
-        <div className="flex absolute right-[40px] hidden items-center gap-x-[5px] border border-[#31603D] p-[2px] px-[6px] pl-[10px] pr-[10px] rounded-[20px] lg:flex text-[#31603D]">
-            <div>Yooo</div>
-            <div>Home</div>
-       </div>
-    <div className="mt-[5px] pt-[20px] flex justify-center">
-   <div className="flex flex-col gap-[20px] lg:flex-row flex-wrap justify-center">
-        <div className="flex flex-col bg-[white] shadow-md border border-[transparent] w-[380px] lg:w-[310px] h-[auto] rounded-[10px]">
-            <div><img src={market.image} className="w-[full] object-cover rounded-tl-[10px] rounded-tr-[10px]"/></div>
-            <div className="pt-[5px] pl-[15px] flex flex-col gap-y-[10px]">
-                <div className="text-[19px] font-bold whitespace-nowrap">{market.name}</div>
-                <div className="flex items-center gap-x-[5px] text-[#31603D]">
-                    <div><LuClock5/></div>
-                    <div className="text-[15px]">Opens {market.opentime} - Closes {market.closetime}</div>
-                </div>
-               <div className="flex flex-wrap items-center mb-[15px]">
-                {market.categories.map((category, index) => (
-              <div className="flex items-center" key={index}>{category}{index < market.categories.length - 1 && <div className="h-[20px] ml-[10px] mr-[10px] border-l border-gray-300 lg:flex justify-between"></div>}</div> 
-            ))}
-            </div>
-            </div>
-        </div>
-</div>
-  </div>
-</>))} */}
+  
 
 
-<div>
+<div className="mt-[30px]">
   {marketss?.length > 0 ? (
     marketss.map((market) => (
       <div key={market.id} className="flex flex-col mb-[30px]">
       
+      <div className="flex items-center">
+        <div>
         <div className="font-bold text-[25px] lg:text-[30px] ml-[20px]">{market.name}</div>
         <div className="w-[270px] lg:w-[100%] md:w-[100%] flex flex-wrap ml-[20px]">
           {market.addr}
         </div>
+        </div>
 
-        <div className="mt-[5px] pt-[20px] flex justify-center">
-          <div className="flex flex-col gap-[20px] lg:flex-row flex-wrap justify-center">
+        <div className="flex items-center gap-x-[20px] absolute right-[30px]">
+         <Link to=""> View all</Link>
+        <div className="hidden lg:flex"><MdOutlineArrowBackIos className="text-[grey] size-[30px]"/></div>
+        <div className="hidden lg:flex"><MdOutlineArrowForwardIos className="size-[30px]"/></div>
+        </div>
+        </div>
+
+        <div className="mt-[5px] pt-[20px] flex lg:flex justify-start">
+
+        <div className="flex overflow-x-scroll scrollbar-hide whitespace-nowrap space-x-4 p-4 lg:flex justify-start">
+          <div className="flex gap-[20px] lg:flex-row justify-center lg:flex justify-start">
             {market.stores.map((store) => (
               <div
                 key={store.id}
-                className="flex flex-col bg-[white] shadow-md border border-[transparent] w-[380px] lg:w-[310px] h-[auto] rounded-[10px]"
+                className="flex flex-col bg-[white] shadow-md border border-[transparent] w-[360px] lg:w-[310px] h-[auto] rounded-[10px] lg:flex justify-start"
               >
             
                 <div>
+                  <div className="h-[150px]">
                   <img
                     src={store.image}
                     alt={store.name}
-                    className="w-full object-cover rounded-tl-[10px] rounded-tr-[10px]"
+                    className="w-full h-full object-cover rounded-tl-[10px] rounded-tr-[10px]"
                   />
+                  </div>
                 </div>
 
                 <div className="pt-[5px] pl-[15px] flex flex-col gap-y-[10px]">
@@ -184,11 +176,15 @@ function LandingPage() {
               </div>
             ))}
           </div>
+
+        </div>
+
+
         </div>
       </div>
     ))
   ) : (
-    <div>No markets available</div>
+    <div></div>
   )}
 </div>
 
