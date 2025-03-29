740e4673c26928bd797254b0df60414782402dce
oladaporoyals <abioyeoladapo@gmail.com>
2025-03-29 23:10:39 +0100

    new update

--- admin-panel/src/components/LayoutButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function LayoutButton({type,text,onClick,children,fill}){
-    return(
-        <div onClick={onClick} className={`px-4 m-0 layoutButton py-1 ${type=="success" ? "success":"danger"} ${fill && "fill"}`}>
-            {text} {children}
-        </div>
-    )
-}
